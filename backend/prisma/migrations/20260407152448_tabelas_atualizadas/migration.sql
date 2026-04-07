9ef84ce1e948a1095b47a3c9d604d9e56faa235a
VinirCavalcante <cavalcant3r1@gmail.com>
2026-04-07 12:44:05 -0300

    Correcoes na migration, e atualizando

--- backend/prisma/migrations/20260407152448_tabelas_atualizadas/migration.sql
+++ backend/prisma/migrations/20260407152448_tabelas_atualizadas/migration.sql
@@ -1,4 +1,14 @@
 -- CreateTable
+CREATE TABLE `Teste` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nome` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `senha` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `Usuario` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `nome` VARCHAR(191) NOT NULL,
@@ -25,10 +35,9 @@ CREATE TABLE `Modulo` (
 -- CreateTable
 CREATE TABLE `Perfil` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `role` ENUM('GERENTE', 'FUNCIONARIO') NOT NULL,
+    `role` ENUM('ADMIN', 'GERENTE', 'FUNCIONARIO') NOT NULL,
     `descricao` VARCHAR(191) NULL,
 
-    UNIQUE INDEX `Perfil_role_key`(`role`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -85,6 +94,27 @@ CREATE TABLE `PermissaoModulo` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `EstoqueAtual` (
+    `materia_prima_id` INTEGER NOT NULL,
+    `quantidade` INTEGER NOT NULL DEFAULT 0,
+
+    PRIMARY KEY (`materia_prima_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Movimentacao` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `materia_prima_id` INTEGER NOT NULL,
+    `tipo` ENUM('ENTRADA', 'SAIDA') NOT NULL,
+    `quantidade` INTEGER NOT NULL,
+    `motivo` VARCHAR(191) NULL,
+    `usuario_id` INTEGER NOT NULL,
+    `data_atual` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `UsuarioPerfil` ADD CONSTRAINT `UsuarioPerfil_usuario_id_fkey` FOREIGN KEY (`usuario_id`) REFERENCES `Usuario`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -102,3 +132,12 @@ ALTER TABLE `PermissaoModulo` ADD CONSTRAINT `PermissaoModulo_perfil_id_fkey` FO
 
 -- AddForeignKey
 ALTER TABLE `PermissaoModulo` ADD CONSTRAINT `PermissaoModulo_modulo_id_fkey` FOREIGN KEY (`modulo_id`) REFERENCES `Modulo`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `EstoqueAtual` ADD CONSTRAINT `EstoqueAtual_materia_prima_id_fkey` FOREIGN KEY (`materia_prima_id`) REFERENCES `MateriaPrima`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Movimentacao` ADD CONSTRAINT `Movimentacao_materia_prima_id_fkey` FOREIGN KEY (`materia_prima_id`) REFERENCES `MateriaPrima`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Movimentacao` ADD CONSTRAINT `Movimentacao_usuario_id_fkey` FOREIGN KEY (`usuario_id`) REFERENCES `Usuario`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
